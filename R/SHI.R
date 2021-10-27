00946bf1b552b10e7ae48cff2f8659a32648a3af
atsyplenkov <atsyplenkov@gmail.com>
2021-10-27 17:42:23 +0300

    some tests

--- R/SHI.R
+++ R/SHI.R
@@ -17,13 +17,13 @@
 #' library(dplyr)
 #' data(djan)
 #' output_table <- hydro_events(dataframe = djan,
-#' q = discharge,
-#' datetime = time,
-#' window = 21)
+#'  q = discharge,
+#'  datetime = time,
+#'  window = 21)
 #'
 #' output_table %>%
-#' filter(he == 2) %>%
-#' SHI(q = discharge, ssc = SS)
+#'  filter(he == 2) %>%
+#'  SHI(q = discharge, ssc = SS)
 #'
 #' @export
 #' @importFrom dplyr "%>%" enquo select
--- tests/testthat/test-check_output.R
+++ tests/testthat/test-check_output.R
@@ -1,9 +1,27 @@
-context("check-output")
 library(testthat)
 library(loadflux)
 
-test_that("hydro_events() returns a data frame", {
-  data(djan)
-  output_table <- hydro_events(dataframe = djan, q = discharge, datetime = time, window = 21)
-  expect_is(output_table, "data.frame")
+test_that("check output classes", {
+
+  output_table <- hydro_events(dataframe = djan,
+                               q = discharge,
+                               datetime = time,
+                               window = 21)
+
+  output_plot <- output_table %>%
+    event_plot(q = discharge,
+               datetime = time,
+               he = he)
+
+  output_ti <- djanturb %>%
+    hydro_events(q = discharge,
+                 datetime = time,
+                 window = 21) %>%
+    dplyr::filter(he == 2) %>%
+    TI(ssc = discharge,
+       datetime = time)
+
+  expect_s3_class(output_plot, c("dygraphs", "htmlwidget"))
+  expect_s3_class(output_plot, c("dygraphs", "htmlwidget"))
+  expect_type(output_ti, "double")
 })
